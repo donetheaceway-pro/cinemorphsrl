3f53c97d9369353cfe111d7f14319fc8b81722bf
Sam <donetheaceway@gmail.com>
2025-11-27 02:01:13 -0500

    Update nova.js

--- nova-engine/nova.js
+++ nova-engine/nova.js
@@ -1,1 +1,12 @@
+export const Nova = {
+  run() {
+    console.log("Nova Engine running...");
+    return "Nova Engine is active.";
+  },
+
+  deploy() {
+    console.log("Deploying via Vercel hook...");
+    return "Deploy triggered.";
+  }
+};
 
